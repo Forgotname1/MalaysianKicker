7729b1c01c7f7a88a957fd43d58c75945a9e70db
Илья Моторов <ilamotorov@gmail.com>
2025-11-26 22:17:16 +0700

    Initial Commit

---
+++ MalaysianRasistTests/MalaysianRasistTests.swift
@@ -0,0 +1,17 @@
+//
+//  MalaysianRasistTests.swift
+//  MalaysianRasistTests
+//
+//  Created by Илья Моторов on 26/11/2568 BE.
+//
+
+import Testing
+@testable import MalaysianRasist
+
+struct MalaysianRasistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
